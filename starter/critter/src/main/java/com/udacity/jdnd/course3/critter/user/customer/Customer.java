fcdce2139f9770209a6ec16a4a89a682c1f98903
Kislay Pandey <kislaypandey1@gmail.com>
2022-09-10 00:39:21 +0530

    Create entity classes for pets, users(employees/customers) and schedules and modify user folder structure

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/customer/Customer.java
@@ -0,0 +1,43 @@
+package com.udacity.jdnd.course3.critter.user.customer;
+
+import com.udacity.jdnd.course3.critter.pet.Pet;
+import com.udacity.jdnd.course3.critter.user.User;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+public class Customer extends User {
+    private String phoneNumber;
+    private String notes;
+
+    @OneToMany(mappedBy = "customer", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Pet> pets;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+}
---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/employee/Employee.java
@@ -0,0 +1,34 @@
+package com.udacity.jdnd.course3.critter.user.employee;
+
+import com.udacity.jdnd.course3.critter.user.User;
+
+import javax.persistence.*;
+import java.time.DayOfWeek;
+import java.util.Set;
+
+@Entity
+public class Employee extends User {
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private Set<EmployeeSkill> skills;
+
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    private Set<DayOfWeek> daysAvailable;
+
+    public Set<EmployeeSkill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Set<EmployeeSkill> skills) {
+        this.skills = skills;
+    }
+
+    public Set<DayOfWeek> getDaysAvailable() {
+        return daysAvailable;
+    }
+
+    public void setDaysAvailable(Set<DayOfWeek> daysAvailable) {
+        this.daysAvailable = daysAvailable;
+    }
+}
